4ae594778a8f666317f22c0b0d152d0788c2d7cf
James <james.humphries2014@hotmail.co.uk>
2024-02-18 17:27:08 +0000

    updated Van Allen

--- AssingmentSheetCalculations/VanAllenBelts.py
+++ AssingmentSheetCalculations/VanAllenBelts.py
@@ -26,13 +26,64 @@ pE1 = [protonMass, protonE1, protonV1]
 pE2 = [protonMass, protonE2, protonV2]
 eE1 = [electronMass, electronE1, electronV1]
 eE2 = [electronMass, electronE2, electronV2]
+lower_bounds = [1.2*Rearth, 4*Rearth]
+upper_bounds = [3*Rearth, 11*Rearth] 
+
 
 poss = [pE1, pE2, eE1, eE2]
+poss_2 = [pE1, pE2, eE1, eE2, lower_bounds, upper_bounds]
+
+B1 = [
+       [protonE1, electronE1],
+      [protonMass, electronMass]
+      ]
+
+B2 = [
+       [protonE2, electronE2],
+       [protonMass, electronMass]
+]
 
 for i in range(0, len(poss)):
        A = poss[i][0]*(poss[i][2]**2)
        B = -G*Mearth*poss[i][0]
        C = -B0*(Rearth**3)*e*poss[i][2]
        print(np.roots([A,B,C])/Rearth)
+
+
+#-------------------------------------
+       
+def MagneticFieldStrength(Lower_Radius, Upper_Radius):
+       lower =  B0 * (Rearth**3) / (Lower_Radius**3)      
+       higher = B0 * (Rearth**3) / (Upper_Radius**3)   
+       return [lower, higher]
+
+
+def LarmorRadius(Energy, Mass, Lower_Radius, Upper_Radius):
+       lower = (np.sqrt(2*Energy*Mass)*(Lower_Radius**3))/(B0*(Rearth**3)*e)
+       upper = (np.sqrt(2*Energy*Mass)*(Upper_Radius**3))/(B0*(Rearth**3)*e) 
+       return [lower/Rearth, upper/Rearth]
+
+# First compute inner belt
+print("----------------------------------------------------- \n Inner Belt:")
+
+for i in range(0, len(B1)):
+       print(LarmorRadius(B1[0][i], B1[1][i], lower_bounds[0], upper_bounds[0]))
+
+# Then compute outer belt
+print("--------------------------------------------------- \n Outer Belt:")
+for i in range(0, len(B2)):
+       print(LarmorRadius(B2[0][i], B2[1][i], lower_bounds[0], upper_bounds[0]))
+
+
+
+
+
+
+
+
+
+
+
+
 
     
